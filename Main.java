5f40d16dcdd0cb1dbe3e507da80c3dda4da24b5b
Monica-Macharia <monicamacharia7@gmail.com>
2024-02-07 05:30:13 +0300

    variable changes

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args){
+        int num = 5;
+        String days = "Wednesday";
+        boolean rainy = true;
+        char letter = 'D';
+        float rounded = 5.7896856f;
+        double truncated = 7.88778686886865556577777777777d;
+
+
+        System.out.println(num + days + rainy +  letter + rounded + truncated);
+    }
+}
